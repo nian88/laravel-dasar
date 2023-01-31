168d9a8b5690fcaea516445c5b470687d8303b18
Azhar Nian <azharnian@gmail.com>
2023-02-01 06:12:11 +0700

    bab route

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,69 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* Basic Route */
+Route::get('/greeting', function () {
+    return 'Selamat Belajar Laravel';
+});
 
-Route::get('/', function () {
-    return view('welcome');
+/* Methode Route */
+Route::get('/route/get', function () {
+    return 'Contoh Route menggunakan Methode GET';
+});
+
+Route::post('/route/post', function () {
+    return 'Contoh Route menggunakan Methode POST';
+});
+
+Route::put('/route/put', function () {
+    return 'Contoh Route menggunakan Methode PUT';
+});
+
+Route::patch('/route/patch', function () {
+    return 'Contoh Route menggunakan Methode PATCH';
+});
+
+Route::delete('/route/delete', function () {
+    return 'Contoh Route menggunakan Methode DELETE';
+});
+
+/* Route Parameter Wajib */
+Route::get('/user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+/* Route Parameter Tidak Wajib */
+Route::get('/profile/{name?}', function ($name = 'John') {
+    return 'Profile Name : '.$name;
+});
+
+/* Route Name */
+Route::get('/list-user', function () {
+    return 'Halaman Class';
+})->name('users');
+
+
+/* Route Without Group */
+Route::get('/student/list', function () {
+    return 'Halaman Daftar Siswa';
+});
+Route::post('/student/store', function () {
+    return 'Halaman Tambah Siswa';
+});
+Route::patch('/student/patch', function () {
+    return 'Halaman Update Siswa';
+});
+
+
+/* Route With Group */
+Route::prefix('student')->group(function () {
+    Route::get('/list', function () {
+        return 'Halaman Daftar Siswa';
+    });
+    Route::post('/store', function () {
+        return 'Halaman Tambah Siswa';
+    });
+    Route::patch('/patch', function () {
+        return 'Halaman Update Siswa';
+    });
 });
